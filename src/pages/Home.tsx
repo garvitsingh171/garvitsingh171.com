75913d2ccaed683782249ce503ab1c2106e939a4
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-07-11 17:43:01 +0530

    feat: build project showcases and category filtering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
-import { HeroSection, StatsSection } from "../components/home";
+import {
+  FeaturedProjectsSection,
+  HeroSection,
+  StatsSection,
+} from "../components/home";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <StatsSection />
+      <FeaturedProjectsSection />
     </>
   );
 }
